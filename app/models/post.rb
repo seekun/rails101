243abe83b293ea9d1ac8523486dd659287036810
seekun <tmsshikun@gmail.com>
2018-10-01 23:13:50 +0800

    using scope to write order by created_by to group's post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+
+  # scope :recent, -> {order{"created_at DESC"}}
+  scope :recent, -> { order("created_at DESC")}
+
 end
